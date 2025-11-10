55d3066892438f8613a63290e817d69360dbdbb3
Gabriel Duarte <gabriel.duarte18@outlook.es>
2025-11-10 18:31:09 -0400

    feat: Refactor LabelPicker to use custom hook for fetching labels and improve loading state

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,39 +1,30 @@
-import { useQuery } from '@tanstack/react-query';
-import { sleep } from '../../helpers';
-
-const getLabels = async (): Promise<any[]> => {
-	// small artificial delay for demo purposes
-	await sleep(1500);
-
-	const res = await fetch(
-		`https://api.github.com/repos/facebook/react/labels`
-	).then((data) => data.json());
-
-	console.log(res);
-
-	return res;
-};
+import { LoadingSpinner } from '../../shared/components/LoadingSpinner';
+import { useLabels } from '../hooks';
 
 export const LabelPicker = () => {
-	const labelsQuery = useQuery({
-		queryKey: ['labels'],
-		queryFn: getLabels,
-	});
+	const { labelsQuery } = useLabels();
 
 	if (labelsQuery.isLoading) {
 		return (
-			<div className='flex justify-center items-center h-52'>Espere...</div>
+			<div className='flex justify-center items-center h-52'>
+				<LoadingSpinner />
+			</div>
 		);
 	}
 
 	return (
-		<>
-			<span
-				className='px-2 py-1 rounded-full text-xs font-semibold hover:bg-slate-800 cursor-pointer'
-				style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-			>
-				Primary
-			</span>
-		</>
+		<div className='flex flex-wrap gap-2 justify-center'>
+			{labelsQuery.data?.map((label) => (
+				<span
+					key={label.id}
+					className='animate-fadeIn px-2 py-1 rounded-full text-xs font-semibold hover:bg-slate-800 cursor-pointer text-white'
+					style={{
+						border: `1px solid #${label.color}`,
+					}}
+				>
+					{label.name}
+				</span>
+			))}
+		</div>
 	);
 };
