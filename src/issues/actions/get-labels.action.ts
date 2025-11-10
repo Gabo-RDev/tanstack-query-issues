d7ca5ce39da31134607f79f7a5317ad590eae60c
Gabriel Duarte <gabriel.duarte18@outlook.es>
2025-11-10 18:30:55 -0400

    feat: Implement getLabels function to fetch GitHub labels with a delay

---
+++ src/issues/actions/get-labels.action.ts
@@ -0,0 +1,14 @@
+import { githubAPI } from '../../api/github.api';
+import { sleep } from '../../helpers';
+import { GithubLabel } from '../interfaces';
+
+export const getLabels = async (): Promise<GithubLabel[]> => {
+	// small artificial delay for demo purposes
+	await sleep(1500);
+
+	const { data } = await githubAPI.get<GithubLabel[]>('/labels');
+
+	console.log(data);
+
+	return data;
+};
