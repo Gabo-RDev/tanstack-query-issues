2879d30eb86a56a45976e42eff86faf0fc6ea028
Gabriel Duarte <gabriel.duarte18@outlook.es>
2025-11-11 13:26:47 -0400

    refactor: Replace hardcoded stale time with constant from helpers

--- src/issues/hooks/useIssue.tsx
+++ src/issues/hooks/useIssue.tsx
@@ -1,17 +1,18 @@
 import { useQuery } from '@tanstack/react-query';
+import { STALE_TIME_MINUTE } from '../../helpers';
 import { getIssue, getIssuesComments } from '../actions';
 
 export const useIssue = (issueNumber: number) => {
 	const issueQuery = useQuery({
 		queryKey: ['issues', issueNumber],
 		queryFn: () => getIssue(issueNumber),
-		staleTime: 1000 * 60,
+		staleTime: STALE_TIME_MINUTE,
 	});
 
 	const commentsQuery = useQuery({
 		queryKey: ['issues', issueQuery.data?.number, 'comments'],
 		queryFn: () => getIssuesComments(issueQuery.data!.number),
-		staleTime: 1000 * 60,
+		staleTime: STALE_TIME_MINUTE,
 		enabled: issueQuery.data !== undefined,
 	});
 
