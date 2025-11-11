02bc5bc673e86a2c154cdc2de8f5b981344bf4c2
Gabriel Duarte <gabriel.duarte18@outlook.es>
2025-11-11 12:51:35 -0400

    refactor: Enhance IssueItem component by adding props for issue data and updating state handling

--- src/issues/components/IssueItem.tsx
+++ src/issues/components/IssueItem.tsx
@@ -1,35 +1,47 @@
-import { FiInfo, FiMessageSquare } from 'react-icons/fi';
+import { FiCheckCircle, FiInfo, FiMessageSquare } from 'react-icons/fi';
 import { useNavigate } from 'react-router-dom';
+import { GithubIssue, State } from '../interfaces';
 
-export const IssueItem = () => {
+interface Props {
+	issue: GithubIssue;
+}
+
+export const IssueItem = ({ issue }: Props) => {
 	const navigate = useNavigate();
 
 	return (
 		<div className='animate-fadeIn flex items-center px-2 py-3 mb-5 border rounded-md bg-slate-900 hover:bg-slate-800'>
-			<FiInfo
-				size={30}
-				color='red'
-				className='min-w-10'
-			/>
+			{issue.state === State.Close ? (
+				<FiCheckCircle
+					size={30}
+					color='green'
+					className='min-w-10'
+				/>
+			) : (
+				<FiInfo
+					size={30}
+					color='red'
+					className='min-w-10'
+				/>
+			)}
 			{/* <FiCheckCircle size={30} color="green" /> */}
 
 			<div className='flex flex-col flex-grow px-2'>
 				<a
-					onClick={() => navigate(`/issues/issue/123`)}
-					className='hover:underline'
+					onClick={() => navigate(`/issues/issue/${issue.number}`)}
+					className='hover:underline cursor-pointer'
 				>
-					Suggestion: why not make accessing and changing the state possible
-					globally?
+					{issue.title}
 				</a>
 				<span className='text-gray-500'>
-					#25581 opened 2 days ago by{' '}
-					<span className='font-bold'>segfaulty1</span>
+					#{issue.number} opened {issue.created_at.toLocaleString()} by{' '}
+					<span className='font-bold'>{issue.user.login}</span>
 				</span>
 			</div>
 
 			<img
-				src='https://avatars.githubusercontent.com/u/1933404?v=4'
-				alt='User Avatar'
+				src={issue.user.avatar_url}
+				alt={issue.user.login}
 				className='w-8 h-8 rounded-full'
 			/>
 			<div className='flex flex-col mx-2 items-center'>
@@ -38,7 +50,7 @@ export const IssueItem = () => {
 					className='min-w-5'
 					color='gray'
 				/>
-				<span className='px-4 text-gray-400'>2</span>
+				<span className='px-4 text-gray-400'>{issue.comments}</span>
 			</div>
 		</div>
 	);
