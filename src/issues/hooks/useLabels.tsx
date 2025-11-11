7d8b931b835fa748ee114e9773fa274c3418f8d2
Gabriel Duarte <gabriel.duarte18@outlook.es>
2025-11-11 12:51:19 -0400

    fix: Update placeholderData structure in useLabels hook

--- src/issues/hooks/useLabels.tsx
+++ src/issues/hooks/useLabels.tsx
@@ -7,26 +7,7 @@ export const useLabels = () => {
 		queryKey: ['labels'],
 		queryFn: getLabels,
 		staleTime: 1000 * 60 * 60, // 1 hour of stale time
-		/* placeholderData: [
-			{
-				id: 6955781886,
-				node_id: 'LA_kwDOAJy2Ks8AAAABnpjO_g',
-				url: 'https://api.github.com/repos/facebook/react/labels/Compiler:%20todo',
-				name: 'Compiler: todo',
-				color: 'C2E0C6',
-				default: false,
-			} satisfies GithubLabel,
-
-			{
-				id: 127893911,
-				node_id: 'MDU6TGFiZWwxMjc4OTM5MTE=',
-				url: 'https://api.github.com/repos/facebook/react/labels/Component:%20DOM',
-				name: 'Component: DOM',
-				color: 'fef2c0',
-				default: false,
-			} satisfies GithubLabel,
-		], */
-		initialData: [
+		placeholderData: [
 			{
 				id: 6955781886,
 				node_id: 'LA_kwDOAJy2Ks8AAAABnpjO_g',
@@ -45,6 +26,25 @@ export const useLabels = () => {
 				default: false,
 			} satisfies GithubLabel,
 		],
+		/* initialData: [
+			{
+				id: 6955781886,
+				node_id: 'LA_kwDOAJy2Ks8AAAABnpjO_g',
+				url: 'https://api.github.com/repos/facebook/react/labels/Compiler:%20todo',
+				name: 'Compiler: todo',
+				color: 'C2E0C6',
+				default: false,
+			} satisfies GithubLabel,
+
+			{
+				id: 127893911,
+				node_id: 'MDU6TGFiZWwxMjc4OTM5MTE=',
+				url: 'https://api.github.com/repos/facebook/react/labels/Component:%20DOM',
+				name: 'Component: DOM',
+				color: 'fef2c0',
+				default: false,
+			} satisfies GithubLabel, 
+		],*/
 	});
 	return { labelsQuery };
 };
